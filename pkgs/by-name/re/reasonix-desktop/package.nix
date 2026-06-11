f188d4aaab2b44a115173e34c05f04f37e30b62d
Mooling0602 <clemooling@outlook.com>
2026-06-11 09:06:00 +0800

    feat(reasonix-desktop): update version

--- pkgs/by-name/re/reasonix-desktop/package.nix
+++ pkgs/by-name/re/reasonix-desktop/package.nix
@@ -14,7 +14,7 @@
 }:
 
 let
-  version = "1.4.0";
+  version = "1.5.0";
   appIcon = fetchurl {
     url = "https://raw.githubusercontent.com/esengine/DeepSeek-Reasonix/desktop-v${version}/desktop/build/appicon.png";
     hash = "sha256-Z3jZuQOz/16ohz+fKEvnn4odE938O70Slr68CdXVgRY=";
@@ -26,7 +26,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/esengine/DeepSeek-Reasonix/releases/download/desktop-v${version}/Reasonix-linux-amd64.tar.gz";
-    hash = "sha256-Q0mAF7sT992qOzfqHSqiEzcBUzaDEcgd8WXj5x+k/BQ=";
+    hash = "sha256-f4Z6OKXacMW/PLd5+xbWrbNTl2CUcG/JrrDI6gQvQv8=";
   };
 
   sourceRoot = ".";
