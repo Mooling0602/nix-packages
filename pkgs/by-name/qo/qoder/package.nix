6d1a6e395b16fb1854d814b1290312bb8b05abf6
Mooling0602 <clemooling@outlook.com>
2026-07-18 17:48:44 +0800

    feat: update version for multiple packages
    Qoder: 1.14.1 -> 1.15.1
    Reasonix Desktop: 1.17.14 -> 1.17.15

--- pkgs/by-name/qo/qoder/package.nix
+++ pkgs/by-name/qo/qoder/package.nix
@@ -43,7 +43,7 @@
 }:
 
 let
-  version = "1.14.1";
+  version = "1.15.1";
 in
 stdenv.mkDerivation {
   pname = "qoder";
@@ -51,7 +51,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://download.qoder.com/release/${version}/qoder_amd64.deb";
-    hash = "sha256-JDmkXfr3dMBg5i+my7i3EUk1sAO8lmhC0tUojIS8Loo=";
+    hash = "sha256-FtpBcGVv8oNbDlhRO1cFDMghJOp8HKxdODc7/mKAqb4=";
   };
 
   nativeBuildInputs = [
--- pkgs/by-name/re/reasonix-desktop/package.nix
+++ pkgs/by-name/re/reasonix-desktop/package.nix
@@ -16,7 +16,7 @@
 }:
 
 let
-  version = "1.17.14";
+  version = "1.17.15";
   gstPlugins = [
     gst_all_1.gst-plugins-base
   ];
@@ -31,7 +31,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/esengine/DeepSeek-Reasonix/releases/download/desktop-v${version}/Reasonix-linux-amd64.tar.gz";
-    hash = "sha256-Xw6PCJmAORLI8tsX5Wrz3s4yJUnelIgxGNPh8Aog+xg=";
+    hash = "sha256-2lKSQzvd7KuOF091EDw3jaR3mH9K5mBBF6yWrWVmnpE=";
   };
 
   sourceRoot = ".";
